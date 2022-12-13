0ca55f30496b957126c4141fc008427373ab9f9b
Pablo de Andres <pablo.de.andres@iwm.fraunhofer.de>
2022-12-13 14:09:07 +0000

    move setup config to setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
+"""This file is required for editable installs of the package."""
 from setuptools import setup
 
-from packageinfo import NAME, VERSION
-
-# Read description
-with open("README.md", "r") as readme:
-    README_TEXT = readme.read()
-
-# main setup configuration class
-setup(
-    name=NAME,
-    version=VERSION,
-    author="Materials Data Science and Informatics Team at Fraunhofer IWM",
-    description="Dataverse repository query",
-    keywords="dataverse repository, Fraunhofer IWM",
-    long_description=README_TEXT,
-    install_requires=[
-        "Flask",
-        "requests_oauthlib",
-        "requests",
-        "rdflib >= 6.0.2, < 7.0.0",
-        "jsonpath-ng",
-        "pycountry",  # For handling ISO 639-1 and ISO 639-2 language codes.
-    ],
-    tests_require=[
-        "pytest==7.1.2",
-    ],
-)
+setup()
